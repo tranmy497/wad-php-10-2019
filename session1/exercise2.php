9d61c4cee2d858cd577a986902ffbc88c3db200a
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2019-09-30 20:06:57 +0700

    Add exercise 2

---
+++ session1/exercise2.php
@@ -0,0 +1,21 @@
+<?php
+
+$n = 3;
+
+if ($n == 2) {
+    echo 'Thứ hai';
+} else if ($n == 3) {
+    echo 'Thứ ba';
+} else if ($n == 4) {
+    echo 'Thứ tư';
+} else if ($n == 5) {
+    echo 'Thứ năm';
+} else if ($n == 6) {
+    echo 'Thứ sáu';
+} else if ($n == 7) {
+    echo 'Thứ bảy';
+} else if ($n == 8) {
+    echo 'Chủ nhật';
+} else {
+    echo 'Không phải ngày trong tuần';
+}
--- session1/learn1.php
+++ session1/learn1.php
@@ -1,11 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tailtq
- * Date: 9/30/19
- * Time: 19:02
- */
-echo 'Hello world';
-?>
-
-<h1>Hello World again</h1>
