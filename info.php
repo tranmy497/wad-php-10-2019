9815c9d443b65c5b9ef763868b4320c96dff9341
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2019-09-30 19:16:32 +0700

    Add session 1

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tailtq
+ * Date: 9/30/19
+ * Time: 18:41
+ */
+
+phpinfo();
---
+++ session1/learn1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tailtq
+ * Date: 9/30/19
+ * Time: 19:02
+ */
+echo 'Hello world';
+?>
+
+<h1>Hello World again</h1>
