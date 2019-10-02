eedfb506665f0288bc72865048bb7b809c1108ce
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2019-10-02 21:23:26 +0700

    Edit form

--- session2/exercises.php
+++ session2/exercises.php
@@ -7,53 +7,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        span {
+        span.error {
             color: red;
+        }
+        p.success {
+            color: #76cf1f;
         }
     </style>
 </head>
 <body>
-<?php
-    if (isset($_POST['register'])) {
-        if ($_POST['email'] && $_POST['name'] && $_POST['password']) {
-            $email = $_POST['email'];
-            $name = $_POST['name'];
-            $password = $_POST['password'];
-            echo "
-                <ul>
-                    <li>Email: $email</li>
-                    <li>Name: $name</li>
-                    <li>Password: $password</li>
-                </ul>
-            ";
-        }
-    }
-?>
+<?php $isSubmitted = isset($_POST['register']); ?>
 <form action="" method="POST">
     <div>
         <label for="">Email: </label>
         <input type="text" name="email">
-        <span><?= isset($_POST['register']) && !$_POST['email'] ? 'This field is required' : '' ?></span>
+        <span class="error"><?= $isSubmitted && !$_POST['email'] ? 'This field is required' : '' ?></span>
     </div>
     <div>
         <label for="">Name: </label>
         <input type="text" name="name">
-        <span><?= isset($_POST['register']) && !$_POST['name'] ? 'This field is required' : '' ?></span>
+        <span class="error"><?= $isSubmitted && !$_POST['name'] ? 'This field is required' : '' ?></span>
     </div>
     <div>
         <label for="">Password: </label>
         <input type="password" name="password">
-        <span><?= isset($_POST['register']) && !$_POST['password'] ? 'This field is required' : '' ?></span>
+        <span class="error"><?= $isSubmitted && !$_POST['password'] ? 'This field is required' : '' ?></span>
     </div>
     <button name="register">Register</button>
+
+    <?php
+    if ($isSubmitted) {
+        if ($_POST['email'] && $_POST['name'] && $_POST['password']) {
+            echo '<p class="success">Register successfully</p>';
+        }
+    }
+    ?>
 </form>
 </body>
 </html>
-<?php
-/**
- * Created by PhpStorm.
- * User: tailtq
- * Date: 10/2/19
- * Time: 21:03
- */
-
